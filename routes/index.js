7538cadc83af581f2b83d972584afa9d297cf59b
Michael  Sewell <MikeSewell01@gmail.com>
2018-02-15 15:00:52 -0500

    empty commit

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 // link to instagram api
 router.get('/v1/gallery', async (req, res)=>{
    const url = "https://www.instagram.com/stylishbarber/?__a=1"
     request(url, (err, output, body)=>{
       body = JSON.parse(body);
-      res.json(body)
+      res.json(body.user.media)
     });
   });
 
