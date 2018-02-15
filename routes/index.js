ff2e4c627de2e07a95c3d8a088e143fdfe12c6cf
Michael  Sewell <MikeSewell01@gmail.com>
2018-02-15 11:42:54 -0500

    added instagram api route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+let request = require("request");
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// link to instagram api
+router.get('/v1/gallery', async (req, res)=>{
+   const url = "https://www.instagram.com/stylishbarber/?__a=1"
+    request(url, (err, output, body)=>{
+      body = JSON.parse(body);
+      res.json(body)
+    });
+  });
 
 module.exports = router;
