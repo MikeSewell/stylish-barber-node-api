9e09e88f4fc6c61291da80cfb26e71a1643bbb98
Michael  Sewell <MikeSewell01@gmail.com>
2018-02-16 18:50:38 -0500

    added event route

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,10 @@ router.get('/v1/gallery', async (req, res)=>{
   // get events from google calender
   router.get('/v1/events', (req, res)=>{
     g.init('events', cb1 = 1, cb2 = 1, cb3 = 1, cb4 = 1, cb5 =1, (err,results)=>{
-       console.log('callback results',results);
-
+      
+      results.forEach(result => {
+         console.log(result.id)
+       });
         res.json(results);
      });
   })
@@ -35,7 +37,7 @@ router.get('/v1/gallery', async (req, res)=>{
   })
   // cancel appointment
   router.post('/v1/event/cancel',(req,res)=>{
-    g.init("cancel", '66h8ni26l2logsobrk2fg5m0mb_20180216T160000Z', 'name and service', '2018-02-16T11:00:00-05:00', '2018-02-16T12:00:00-05:00', 'opaque', (err, results) => {
+    g.init("cancel", '66h8ni26l2logsobrk2fg5m0mb_20180216T160000Z', 'name and service', '2018-02-16T11:00:00-05:00', '2018-02-16T12:00:00-05:00', 'transparent', (err, results) => {
       console.log(err);
 
       res.json(results);
